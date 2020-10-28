43caf693b03f271f2c97b3b56b9eac1d802d10b3
JerryJSpringer <JerryJSpringer@gmail.com>
2020-10-27 17:19:34 -0700

    New main activity so authentication and main are separate.

--- app/src/main/java/edu/uw/comchat/MainActivity.java
+++ app/src/main/java/edu/uw/comchat/MainActivity.java
@@ -1,13 +1,37 @@
 package edu.uw.comchat;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            navController.navigate(R.id.settingsFragment);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
