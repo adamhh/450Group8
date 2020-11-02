4a7f17cda6736e4b6c65f79831f142bbb3b3b44f
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-02 15:40:34 -0800

    Created a card fragment for use in the weather fragment.

---
+++ app/src/main/java/edu/uw/comchat/ui/weather/WeatherCardFragment.java
@@ -0,0 +1,36 @@
+package edu.uw.comchat.ui.weather;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.uw.comchat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WeatherCardFragment extends Fragment {
+
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_card, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        ((TextView) view.findViewById(R.id.test))
+                .setText(Integer.toString(args.getInt(ARG_OBJECT)));
+    }
+}
