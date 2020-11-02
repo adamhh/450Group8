f48c3c9686525809182ec8f63d66a8fa6d071f1a
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-02 15:58:53 -0800

    Added a toolbar.

--- app/src/main/java/edu/uw/comchat/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/comchat/ui/home/HomeFragment.java
@@ -2,9 +2,12 @@ package edu.uw.comchat.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,7 +21,13 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.toolbar_home, menu);
     }
 }
