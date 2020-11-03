5d4c13800e07995ca3b54ddd4c4b12f4516d44d5
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-02 16:07:35 -0800

    A card for use in the list that displays a users conversations.

---
+++ app/src/main/java/edu/uw/comchat/ui/chat/CreateFragment.java
@@ -0,0 +1,81 @@
+package edu.uw.comchat.ui.chat;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.comchat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+
+    /*
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onResume() {
+        super.onResume();
+        ActionBar supportActionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        supportActionBar.setShowHideAnimationEnabled(false);
+        if (supportActionBar != null)
+            supportActionBar.hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ActionBar supportActionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        if (supportActionBar != null)
+            supportActionBar.show();
+    }
+
+     */
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.toolbar_create, menu);
+    }
+
+    /*
+    private ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater inflater = actionMode.getMenuInflater();
+            inflater.inflate(R.menu.toolbar_context_message, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            actionMode = null;
+        }
+    };
+
+     */
+}
