fe584695347ea1326043034e3c5b17632e3a9987
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-03 15:57:41 -0800

    Added to represent a card in the connection list view.

---
+++ app/src/main/java/edu/uw/comchat/ui/connection/ConnectionCardFragment.java
@@ -0,0 +1,29 @@
+package edu.uw.comchat.ui.connection;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.comchat.R;
+
+/**
+ * A fragment that represents a single card for a connection
+ * in the connections list for users. The lists are for existing,
+ * outgoing connection requests, and incoming connection requests.
+ *
+ * @author Jerry Springer
+ *  @version 3 November 2020
+ */
+public class ConnectionCardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_connection_card, container, false);
+    }
+}
