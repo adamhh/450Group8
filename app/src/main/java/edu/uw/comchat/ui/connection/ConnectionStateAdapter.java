762641b7843baef0e867ede98b58ec7312c21158
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-03 16:04:20 -0800

    State adapter for use with the tabular connection layout.

---
+++ app/src/main/java/edu/uw/comchat/ui/connection/ConnectionStateAdapter.java
@@ -0,0 +1,37 @@
+package edu.uw.comchat.ui.connection;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+/**
+ * The state adapter that is used with the connection tab layout.
+ *
+ * @author Jerry Springer
+ * @version 3 November 2020
+ */
+public class ConnectionStateAdapter extends FragmentStateAdapter {
+
+    /**
+     * Creates a new connection state adapter from the given fragment.
+     *
+     * @param fragment the fragment containing the state adapter.
+     */
+    public ConnectionStateAdapter(Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        // TODO Set the connection list up properly
+        Fragment fragment = new ConnectionListFragment();
+        return fragment;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
