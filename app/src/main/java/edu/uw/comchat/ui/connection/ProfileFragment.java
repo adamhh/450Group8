71423feaa5364b0bf719ed830455906f5fc80187
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-03 16:10:10 -0800

    Added the profile fragment.

---
+++ app/src/main/java/edu/uw/comchat/ui/connection/ProfileFragment.java
@@ -0,0 +1,27 @@
+package edu.uw.comchat.ui.connection;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.comchat.R;
+
+/**
+ * A fragment that is used to show a profile.
+ *
+ * @author Jerry Springer
+ * @version 3 November 2020
+ */
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
