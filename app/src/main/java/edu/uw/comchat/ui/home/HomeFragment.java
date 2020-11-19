f8e6d82326512c139a8c8e9821788d968f272918
Hung Vu <hunghvu2017@gmail.com>
2020-11-18 20:52:39 -0800

    Checkstyle home package

--- app/src/main/java/edu/uw/comchat/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/comchat/ui/home/HomeFragment.java
@@ -26,11 +26,5 @@ public class HomeFragment extends Fragment {
     return inflater.inflate(R.layout.fragment_home, container, false);
   }
 
-  // Setting across all fragments, should be in activity instead? - Hung Vu
-//  @Override
-//  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-//    super.onCreateOptionsMenu(menu, inflater);
-//    getActivity().getMenuInflater().inflate(R.menu.toolbar_home, menu);
-//  }
-  // Checkstyle: Done - Hung Vu
+  // Checkstyle done, sprint 2 - Hung Vu. Ignore member name errors if they exist.
 }
