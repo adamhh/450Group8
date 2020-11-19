8d9bb260d1e206c6a0a7f604a2522b824827a1c3
JerryJSpringer <JerryJSpringer@gmail.com>
2020-11-19 06:17:16 -0800

    Removed some strings from the MainActivity and moved them into the theme class. Changed all mutable fields to private and added additional methods to provide functionality. Also added a method for getting the theme name for the dialogue picker though this should probably be changed.

--- app/src/main/java/edu/uw/comchat/Theme.java
+++ app/src/main/java/edu/uw/comchat/Theme.java
@@ -1,11 +1,28 @@
 package edu.uw.comchat;
 
 public class Theme {
-    public static int mTheme = R.style.Theme_ComChat;
-    public static boolean mIsDark = false;
+    public static final String DEFAULT_THEME = "default";
+    public static final String GREY_THEME = "grey";
+    public static final String RED_THEME = "red";
+
+    private static String mThemeName = DEFAULT_THEME;
+    private static int mTheme = R.style.Theme_ComChat;
+    private static boolean mIsDark = false;
+
+    public static void toggleDark() {
+        mIsDark = !mIsDark;
+    }
+
+    public static boolean getIsDark() {
+        return mIsDark;
+    }
 
     public static int getTheme() {
         return mTheme;
+    }
+
+    public static String getThemeName() {
+        return mThemeName;
     }
 
     public static void setTheme(String description) {
@@ -14,18 +31,17 @@ public class Theme {
         switch (str) {
             case "red":
                 result = R.style.Theme_ComChatRed;
+                mThemeName = RED_THEME;
                 break;
             case "grey":
                 result = R.style.Theme_ComChatBlueGrey;
+                mThemeName = GREY_THEME;
                 break;
             default:
                 result = R.style.Theme_ComChat;
+                mThemeName = DEFAULT_THEME;
                 break;
         }
         mTheme = result;
-
     }
-
-
-
 }
