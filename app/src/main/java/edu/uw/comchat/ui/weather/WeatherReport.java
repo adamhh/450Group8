4017e88034c2cd7f1b08cc6b0fbe30043ac874f4
JerryJSpringer <JerryJSpringer@gmail.com>
2020-12-07 14:22:34 -0800

    Added additional information to the weather report.

--- app/src/main/java/edu/uw/comchat/ui/weather/WeatherReport.java
+++ app/src/main/java/edu/uw/comchat/ui/weather/WeatherReport.java
@@ -1,12 +1,22 @@
 package edu.uw.comchat.ui.weather;
 
 public class WeatherReport {
-    private String mTime;
-    private String mTemp;
+    private final String mTime;
+    private final String mTemp;
+    private final String mSunrise;
+    private final String mSunset;
+    private final int mId;
 
-    public WeatherReport(String time, String temp) {
+    public WeatherReport(String time, String temp, String sunrise, String sunset, int id) {
         mTime = time;
         mTemp = temp;
+        mSunrise = sunrise;
+        mSunset = sunset;
+        mId = id;
+    }
+
+    public WeatherReport(String time, String temp, int id) {
+        this(time, temp, "", "", id);
     }
 
     public String getTime() {
@@ -15,5 +25,17 @@ public class WeatherReport {
 
     public String getTemp() {
         return mTemp;
+    }
+
+    public String getSunrise() {
+        return mSunrise;
+    }
+
+    public String getSunset() {
+        return mSunset;
+    }
+
+    public int getId() {
+        return mId;
     }
 }
