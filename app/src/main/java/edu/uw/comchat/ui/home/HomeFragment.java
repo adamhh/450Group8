8b462c36d8896465d70426bcd87a52fca6785b4a
JerryJSpringer <JerryJSpringer@gmail.com>
2020-12-09 19:41:01 -0800

    Updated home page UI.

--- app/src/main/java/edu/uw/comchat/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/comchat/ui/home/HomeFragment.java
@@ -1,40 +1,45 @@
 package edu.uw.comchat.ui.home;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.SnapHelper;
+
 import edu.uw.comchat.R;
 import edu.uw.comchat.databinding.FragmentHomeBinding;
 import edu.uw.comchat.model.NotificationViewModel;
 import edu.uw.comchat.model.UserInfoViewModel;
+import edu.uw.comchat.ui.weather.WeatherReport;
 import edu.uw.comchat.ui.weather.WeatherViewModel;
 import edu.uw.comchat.util.StorageUtil;
 
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.TimeZone;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-
-
-
 /**
  * A fragment for the users home view.
  *
  * @author Jerry Springer, Hung Vu
- * @version 3 November 2020
+ * @version 9 December 2020
  */
 // Ignore checkstyle member name error.
 public class HomeFragment extends Fragment {
@@ -43,7 +48,6 @@ public class HomeFragment extends Fragment {
   private FragmentHomeBinding mBinding;
 
   private NotificationViewModel mNotificationModel;
-  // TODO adjust home fragment, it's just in a bare bone state now.
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,7 +60,7 @@ public class HomeFragment extends Fragment {
   }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     setHasOptionsMenu(true);
     mBinding = FragmentHomeBinding.inflate(inflater);
@@ -66,19 +70,42 @@ public class HomeFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    mBinding.textHomeUserEmail.setText(mUserModel.getEmail());
+
+    // Sets user email
+    String hello = getString(R.string.text_home_hello) + " " + mUserModel.getEmail();
+    mBinding.textHomeUserEmail.setText(hello);
+
+    // Set up weather connection
     mWeatherModel.addResponseObserver(getViewLifecycleOwner(), this::observeResponse);
     StorageUtil util = new StorageUtil(getContext());
     mWeatherModel.connect(util.getLocation());
 
+    // Sets layout for recycler view
+    SnapHelper helper = new LinearSnapHelper();
+    helper.attachToRecyclerView(mBinding.listHomeWeather);
+    mBinding.listHomeWeather.setLayoutManager(new LinearLayoutManager(
+            getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+    // Set scroll behaviour for notification message
+    mBinding.textHomeWelcomeMessage.setMovementMethod(new ScrollingMovementMethod());
+
+    // Notification response observer
     mNotificationModel.addResponseObserver(getViewLifecycleOwner(), notificationMap -> {
       try {
-        mBinding.textHomeNotificationMessage.setText("Message: " + mNotificationModel.getLatestNotificationMessage());
-        mBinding.textHomeNotificationSender.setText("From: " + mNotificationModel.getLatestNotificationSender());
-        mBinding.textHomeNotificationDate.setText("Date: " + mNotificationModel.getLatestNotificationDate());
-        mBinding.textHomeNotificationTime.setText("Time: " + mNotificationModel.getLatestNotificationTime());
+        // Show notification and hide display message
+        mBinding.textHomeNotificationDisplay.setVisibility(View.INVISIBLE);
+        mBinding.layoutInnerHomeNotification.setVisibility(View.VISIBLE);
+
+        mBinding.textHomeNotificationMessage.setText(mNotificationModel.getLatestNotificationMessage());
+        mBinding.textHomeNotificationSender.setText(mNotificationModel.getLatestNotificationSender());
+        mBinding.textHomeNotificationDate.setText(mNotificationModel.getLatestNotificationDate());
+        mBinding.textHomeNotificationTime.setText(mNotificationModel.getLatestNotificationTime());
       } catch (NullPointerException e){
         Log.i("Home Fragment", "NPE for at notification due to no message has been received, this error can be ignored.");
+
+        // Hide empty notification and show display
+        mBinding.layoutInnerHomeNotification.setVisibility(View.INVISIBLE);
+        mBinding.textHomeNotificationDisplay.setVisibility(View.VISIBLE);
       }
     });
   }
@@ -98,7 +125,7 @@ public class HomeFragment extends Fragment {
         }
       } else {
         try {
-          currentWeatherInfo(response);
+          populateWeatherInfo(response);
         } catch (JSONException e) {
           e.printStackTrace();
         }
@@ -114,14 +141,13 @@ public class HomeFragment extends Fragment {
    * @param response web service response
    * @throws JSONException an exception handled by observeResponse
    */
-  private void currentWeatherInfo(JSONObject response) throws JSONException {
+  private void populateWeatherInfo(JSONObject response) throws JSONException {
     JSONObject currentWeather = response.getJSONObject("current");
-    JSONArray weatherDescription = currentWeather.getJSONArray("weather");
-    int weatherDescriptionId = weatherDescription.getJSONObject(0).getInt("id");
-    long sunriseTime = currentWeather.getLong("sunrise");
-    long sunsetTime = currentWeather.getLong("sunset");
+    JSONObject weatherObject = currentWeather.getJSONArray("weather").getJSONObject(0);
 
     // Get sunrise and sunset time.
+    long sunriseTime = currentWeather.getLong("sunrise");
+    long sunsetTime = currentWeather.getLong("sunset");
     final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
     final String formattedSunriseTime = Instant.ofEpochSecond(sunriseTime)
             .atZone(TimeZone.getDefault().toZoneId())
@@ -130,22 +156,21 @@ public class HomeFragment extends Fragment {
             .atZone(TimeZone.getDefault().toZoneId())
             .format(formatter);
 
-    String currentTemp = currentWeather.getString("temp") + " F";
+    // Set location
+    String location = response.getString("City")
+            + ", " + response.getString("State")
+            + ", " + response.getString("Country");
+    mBinding.textHomeWeatherLocation.setText(location);
 
-    setWeatherInfo(weatherDescriptionId, formattedSunriseTime,
-            formattedSunsetTime, currentTemp);
+    // Set temp
+    String currentTemp = currentWeather.getString("temp") + "\u00B0";
+    mBinding.textHomeWeatherCurrent.setText(currentTemp);
 
-  }
+    // Get description
+    mBinding.textHomeWeatherDescription.setText(weatherObject.getString("description"));
 
-  /**
-   * This helper method helps modify UI to display weather information.
-   *
-   * @param id          weather description id
-   * @param sunriseTime sunrise time
-   * @param sunsetTime  sunset time
-   * @param currentTemp current temperature
-   */
-  private void setWeatherInfo(int id, String sunriseTime, String sunsetTime, String currentTemp) {
+    // Get image icon using weather id
+    int id = weatherObject.getInt("id");
     if (id >= 200 && id <= 232) { // ID for thunderstorm
       mBinding.imageViewHomeWeather.setImageResource(R.drawable.ic_home_thunder_storm);
     } else if (id >= 300 && id <= 321) { // ID for drizzle
@@ -158,7 +183,7 @@ public class HomeFragment extends Fragment {
       mBinding.imageViewHomeWeather.setImageResource(R.drawable.ic_home_fog);
     } else if (id == 800) { // ID for clear, icon varies based on time in a day
       LocalTime currentTime = LocalTime.now();
-      if (currentTime.isAfter(LocalTime.parse(sunriseTime)) && currentTime.isBefore(LocalTime.parse(sunsetTime))) {
+      if (currentTime.isAfter(LocalTime.parse(formattedSunriseTime)) && currentTime.isBefore(LocalTime.parse(formattedSunsetTime))) {
         mBinding.imageViewHomeWeather.setImageResource(R.drawable.ic_home_clear_sky_day);
       } else {
         mBinding.imageViewHomeWeather.setImageResource(R.drawable.ic_home_clear_sky_night);
@@ -166,8 +191,26 @@ public class HomeFragment extends Fragment {
     } else if (id >= 801 && id <= 804) { // ID for cloud
       mBinding.imageViewHomeWeather.setImageResource(R.drawable.ic_home_cloudy);
     }
-    mBinding.textHomeWeatherTemp.setText(currentTemp);
 
+    // Set recycler view
+    ArrayList<WeatherReport> dailyReports = new ArrayList<>();
+    JSONArray dailyArray = response.getJSONArray("daily");
+    try {
+      for (int i = 0; i < dailyArray.length(); i++) {
+        JSONObject json = dailyArray.getJSONObject(i);
+        JSONObject time = json.getJSONObject("dt");
+        JSONObject weather = json.getJSONArray("weather").getJSONObject(0);
+        dailyReports.add(new WeatherReport(
+                time.getString("months") + "/" + time.getString("dates"),
+                json.getJSONObject("temp").getString("day") + "\u00B0",
+                weather.getString("description"),
+                weather.getInt("id")));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    mBinding.listHomeWeather.setAdapter(new HomeWeatherRecyclerViewAdapter(dailyReports));
   }
 
   @Override
@@ -175,6 +218,4 @@ public class HomeFragment extends Fragment {
     super.onCreateOptionsMenu(menu, inflater);
     getActivity().getMenuInflater().inflate(R.menu.toolbar_home, menu);
   }
-
-
 }
